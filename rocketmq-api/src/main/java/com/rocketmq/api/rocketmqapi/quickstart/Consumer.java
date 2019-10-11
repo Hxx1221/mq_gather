fe45b3b11098e847212ef3d033198ad891f9266c
hexixiang <137125668@qq.com>
2019-10-11 19:57:34 +0800

    7777

--- rocketmq-api/src/main/java/com/rocketmq/api/rocketmqapi/quickstart/Consumer.java
+++ rocketmq-api/src/main/java/com/rocketmq/api/rocketmqapi/quickstart/Consumer.java
@@ -8,6 +8,7 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 import java.util.List;
@@ -16,16 +17,17 @@ public class Consumer {
     public static void main(String[] args) throws MQClientException {
 
         DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer("test_quick_consumer");
+       // defaultMQPushConsumer.setMessageModel(MessageModel.CLUSTERING);//集群模式
+        defaultMQPushConsumer.setMessageModel(MessageModel.BROADCASTING);
 
         defaultMQPushConsumer.setNamesrvAddr(Constants.NAMESRV_ADDR);
-        defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
-        defaultMQPushConsumer.subscribe("test_quick_topic", "*");
+        defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        defaultMQPushConsumer.subscribe("test_quick_topic", "TagA||TagB");
 
         defaultMQPushConsumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
                 MessageExt me = list.get(0);
-
                 try {
 
                     String topic = me.getTopic();
@@ -33,10 +35,13 @@ public class Consumer {
                     byte[] body = me.getBody();
                     String s = new String(body, RemotingHelper.DEFAULT_CHARSET);
                     String keys = me.getKeys();
-                    if (keys.equals("keyA1")) {
-                        System.out.println("topic:" + topic + "," + "tags:" + tags + "," + "keys:" + keys + "," + "body:" + s);
-                        int a = 1 / 0;
-                    }
+                    int queueId = me.getQueueId();
+                    long queueOffset = me.getQueueOffset();
+                    long commitLogOffset = me.getCommitLogOffset();
+//                    if (keys.equals("keyA1")) {
+                        System.out.println("queueOffset:"+queueOffset+" queueId:"+queueId+"  topic:" + topic + "," + "tags:" + tags + "," + "keys:" + keys + "," + "body:" + s);
+//                        int a = 1 / 0;
+//                    }
                 } catch (Exception e) {
                     int reconsumeTimes = me.getReconsumeTimes();//重试次数
                     if (reconsumeTimes == 3) {
--- rocketmq-api/src/main/java/com/rocketmq/api/rocketmqapi/quickstart/Producer.java
+++ rocketmq-api/src/main/java/com/rocketmq/api/rocketmqapi/quickstart/Producer.java
@@ -4,9 +4,12 @@ import com.rocketmq.api.rocketmqapi.constants.Constants;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.List;
 
 public class Producer {
 
@@ -18,15 +21,47 @@ public class Producer {
                 new DefaultMQProducer("test_quickstart");
         defaultMQProducer.setNamesrvAddr(Constants.NAMESRV_ADDR);
         defaultMQProducer.start();
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i < 50; i++) {
+            String tag= i%2==0?"TagA":"TagB";
+
             Message message = new Message("test_quick_topic",//主题
-                    "TagA",//标签
-                    "keyA"+i,//用户自定义的key 唯一的标识
-                    ("hxxlakjsf"+i).getBytes());//消息内容实体
-            SendResult send = defaultMQProducer.send(message);
-            System.out.println("消息发出："+send);
+                    tag,//标签
+                    "keyA" + i,//用户自定义的key 唯一的标识
+                    ("hxxlakjsf" + i).getBytes());//消息内容实体
+            //1.同步发送消息
+//            SendResult send = defaultMQProducer.send(message);
+            message.setDelayTimeLevel(1);
+            //2.异步发送消息
+            defaultMQProducer.send(message,
+//                    new MessageQueueSelector() {
+//                @Override
+//                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+//                    Integer s= (Integer) arg;
+//                //    System.out.println(s);
+//                    MessageQueue messageQueue = mqs.get(s);
+//                    return messageQueue;
+//                }
+//            },2,
+                    new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+//                    生成者发送消息返回的状态
+//                            SEND_OK,  发送成功
+//                    FLUSH_DISK_TIMEOUT, 刷盘超时
+//                    FLUSH_SLAVE_TIMEOUT, 从节点同步超时
+//                    SLAVE_NOT_AVAILABLE,  从节点不可用
+                    System.out.println("msgId:" + sendResult.getMsgId() + " status:" + sendResult.getSendStatus());
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+                    System.err.println("发送失败");
+                }
+            });
+
+
         }
-        defaultMQProducer.shutdown();
+        //defaultMQProducer.shutdown();
 
     }
 }
